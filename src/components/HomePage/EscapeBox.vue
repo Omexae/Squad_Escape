367623c52249906397f888fcba3e0368da736c1d
BenRoecker <benjamin.roecker@efrei.net>
2021-10-11 18:04:18 +0200

    test

---
+++ src/components/HomePage/EscapeBox.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="Box">
+    <h2>{{ title }}</h2>
+    <p>{{ prix }}</p>
+  </div>
+  <div class="hidden">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HscapeBox",
+  components: {},
+  props: {
+    title: String,
+    message: String,
+    prix: Int32Array,
+    image: String,
+  },
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="home">
+    <EscapeBox title="FUN" message="Fun" prix="67" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import EscapeBox from "../components/HomePage/EscapeBox.vue";
 export default {
   name: "Home",
-  components: {},
+  components: {
+    EscapeBox,
+  },
 };
 </script>
