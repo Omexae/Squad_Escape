7fc48b0aaeebad4655d289bb4e33ab58d20af46c
Antoine RECIO ANDRADES <antoine.recio.andrades@efrei.net>
2021-10-11 18:04:01 +0200

    Footer et header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
-  <div id="nav"></div>
+  <div id="nav">
+    <base-header></base-header>
+    <base-footer></base-footer>
+  </div>
 </template>
-
+<script>
+import BaseHeader from "./components/BaseHeader.vue";
+import BaseFooter from "./components/BaseFooter.vue";
+export default({
+  name: 'App',
+  components:{BaseHeader,BaseFooter}
+})
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -10,16 +20,13 @@
   text-align: center;
   color: #2c3e50;
 }
-
 #nav {
   padding: 30px;
 }
-
 #nav a {
   font-weight: bold;
   color: #2c3e50;
 }
-
 #nav a.router-link-exact-active {
   color: #42b983;
 }
---
+++ src/components/BaseFooter.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer class="base-footer">
+    Property of ROECKER Benjamin and Antoine RECIO ANDRADES | © 2021 
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'BaseFooter'
+}
+</script>
+
+<style scoped>
+.base-footer {
+  background-color: #020059;
+  padding: 0.5em;
+  text-align: center;
+  color: white;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+a {
+  color: white;
+}
+</style>
