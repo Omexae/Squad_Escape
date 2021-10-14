74148ec519ab989a0f304b879d4f82551b677ccb
BenRoecker <benjamin.roecker@efrei.net>
2021-10-14 17:40:51 +0200

    Ajout design boite escape

--- src/components/HomePage/EscapeBox.vue
+++ src/components/HomePage/EscapeBox.vue
@@ -1,24 +1,54 @@
 <template>
-  <div class="Box">
-    <h2>{{ title }}</h2>
-    <p>{{ prix }}</p>
-  </div>
-  <div class="hidden">
-    <p>{{ message }}</p>
+  <div :style="'background-image : url(' + url + ');'" id="visible" class="box">
+    <div>
+      <h2>{{ title }}</h2>
+      <p>{{ prix }} euro</p>
+    </div>
+    <div class="hidden">
+      <p>{{ message }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "HscapeBox",
+  name: "EscapeBox",
   components: {},
   props: {
     title: String,
     message: String,
     prix: Int32Array,
-    image: String,
+    url: String,
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+.box {
+  background-color: #cccccc;
+  border-radius: 25px;
+  box-shadow: #999999 5px 5px 10px, #eeeeee -5px -5px 10px;
+  width: 300px;
+  color: white;
+}
+
+.hidden {
+  background-color: white;
+  width: 270px;
+  padding: 5px;
+  color: black;
+}
+#visible {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+.hidden {
+  display: flex;
+  width: 100%;
+  border-radius: 25px;
+}
+.box:hover {
+  box-shadow: #999999 -5px -5px 10px, #eeeeee 5px 5px 10px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,19 @@
-<template>
+<template id="page">
   <div class="home">
-    <EscapeBox title="FUN" message="Fun" prix="67" />
+    <EscapeBox
+      class="Box"
+      title="FUN"
+      message="C'est un escape game assez fun mais pas très drole."
+      prix="78"
+      url="https://images.unsplash.com/photo-1605870445919-838d190e8e1b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1172&q=80"
+    />
+    <EscapeBox
+      class="Box"
+      title="FUN"
+      message="C'est un escape game assez fun mais pas très drole."
+      prix="78"
+      url="https://images.unsplash.com/photo-1605870445919-838d190e8e1b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1172&q=80"
+    />
   </div>
 </template>
 
@@ -14,3 +27,16 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.home {
+  background-color: #cccccc;
+  width: 100%;
+  height: 100%;
+  display: flex;
+}
+.Box {
+  padding: 20px;
+  margin: 20px;
+}
+</style>
