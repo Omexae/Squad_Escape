8b6db94db410d8d1a01339752a73915966968df0
Antoine RECIO ANDRADES <antoine.recio.andrades@efrei.net>
2021-10-15 16:56:47 +0200

    Creation pour créér un compte

---
+++ src/components/CreateAccount.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <div id="Form">
+            <form>
+                <label>Username</label>
+                <input placeholder="Enter your username">
+                <label>password</label>
+                <input placeholder="Enter your password">
+                <label>Confirm password</label>
+                <input placeholder="Confirm password">
+            </form>
+        </div>
+    </div>
+</template>
