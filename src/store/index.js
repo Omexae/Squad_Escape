942b7c5490824efb18d7febb9e08fda63d67d33a
BenRoecker <benjamin.roecker@efrei.net>
2021-11-07 15:28:42 +0100

    changementy

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,7 @@ import escapes from "./modules/escapes";
 import reservation from "./modules/reservation";
 export default createStore({
   state: {
-<<<<<<< HEAD
     isLoggedIn: false,
-=======
-    IsLoggedIn: false,
->>>>>>> 356a38e3293ef9ad636872b78535d0d9b97df723
   },
   mutations: {},
   actions: {},
